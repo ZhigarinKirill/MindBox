39c52944273a54b06591f327aa25e8ba170c909d
zhiga <zhigarink@gmail.com>
2022-10-15 13:35:52 +0300

    added restrictions on max id

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def num_sum(num: int) -> int:
 
 def count_by_groups(n_customers: int) -> Dict[int, int]:
     groups = defaultdict(int)
-    for id in range(n_customers):
+    for id in range(min(9999999, n_customers)):
         groups[num_sum(id) % 64] += 1
     return groups
 
@@ -16,7 +16,7 @@ def count_by_groups(n_customers: int) -> Dict[int, int]:
 # С параметром по-умолчанию равнозначна count_by_groups
 def count_by_groups_with_first_id(n_customers: int, n_first_id: int = 0) -> Dict[int, int]:
     groups = defaultdict(int)
-    for id in range(n_first_id, n_first_id + n_customers):
+    for id in range(n_first_id, min(9999999, n_first_id + n_customers)):
         groups[num_sum(id) % 64] += 1
     return groups
 
